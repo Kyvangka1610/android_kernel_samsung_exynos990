4ae84cd8d49a87dbebe1e77ad2e1c09762b01859
Quentin Perret <qperret@google.com>
2021-10-18 14:31:38 +0530

    cpufreq: Register governors at core_initcall
    Currently, most CPUFreq governors are registered at the core_initcall
    time when the given governor is the default one, and the module_init
    time otherwise.
    
    In preparation for letting users specify the default governor on the
    kernel command line, change all of them to be registered at the
    core_initcall unconditionally, as it is already the case for the
    schedutil and performance governors. This will allow us to assume
    that builtin governors have been registered before the built-in
    CPUFreq drivers probe.
    
    And since all governors have similar init/exit patterns now, introduce
    two new macros, cpufreq_governor_{init,exit}(), to factorize the code.
    
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    [ rjw: Changelog ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1065,11 +1065,7 @@ struct cpufreq_governor *cpufreq_default_governor(void)
 }
 #endif
 
-static int __init sugov_register(void)
-{
-	return cpufreq_register_governor(&schedutil_gov);
-}
-fs_initcall(sugov_register);
+cpufreq_governor_init(schedutil_gov);
 
 #ifdef CONFIG_ENERGY_MODEL
 extern bool sched_energy_update;
